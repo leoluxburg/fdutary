8d72f8bdbba1ab264f43f82e440133b0384e9358
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2020-05-11 22:05:36 -0500

    video update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
  get '/cinematography', to: 'pages#cinematography'
  get '/panchito', to: 'pages#panchito'
   get '/contact', to: 'pages#contact'
-  get '/records', to: 'pages#records'
+  get '/archivos', to: 'pages#records'
    get 'download_pdf', to: "pages#download_pdf"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
